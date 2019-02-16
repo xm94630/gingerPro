7e52839deecfd224b0970d7f96d463e5c9c893b0
xm94630 <xm94630@126.com>
2019-02-16 10:12:54 +0800

    代码没有错误了，但是数据库创建表还没有，再查查

--- app/config/secure.py
+++ app/config/secure.py
@@ -1,4 +1,9 @@
 SQLALCHEMY_DATABASE_URI = \
     'mysql+cymysql://root:xm123123@localhost/ginger'
 
-SECRET_KEY = '123'
+#SECRET_KEY = '\x88D\xf09\x6\xa0A\x7\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x98*4'
+
+
+#有个警告，用这个消除
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
