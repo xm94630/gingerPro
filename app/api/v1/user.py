039cd622ad8cf8f48e1a28ba55beb0babde9126c
xm94630 <xm94630@126.com>
2019-02-19 11:10:35 +0800

    mod

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -7,14 +7,16 @@ from app.libs.token_auth import auth
 # user = Blueprint('user',__name__)
 
 # redprint
-
-
+from models.user import User
 
 api = Redprint('user')
 
-@api.route('/get')
+@api.route('/<int:uid>')
 @auth.login_required
-def get_user():
+def get_user(uid):
+    user = User.query.get_or_404(uid)
+    print('--->??')
+    print(user)
     return '泰罗'
 
 # @api.route('/create')
