0f3b1b9921be9d393d734b5e2d9c0f9fcba8a0fb
xm94630 <xm94630@126.com>
2019-02-26 10:55:43 +0800

    设置 allow_module 列表，这样子可对一个模块下的所有视图函数进行访问

--- app/libs/scope.py
+++ app/libs/scope.py
@@ -6,14 +6,18 @@ class Scope:
     #运算符重置，__add__是内置的方法。相当于都"+"号做了新的定义
     def __add__(self,other):
         self.allow_api = self.allow_api + other.allow_api
+        # set 定义集合元素，不重复，相当于去重
+        self.allow_api = list(set(self.allow_api))
         return self
 
 
 class AdminScope(Scope):
-    allow_api=['v1.super_get_user']
+    #allow_api=['v1.super_get_user']
+    allow_api=[]
+    allow_module = ['v1.user']
     def __init__(self,):
         #self.add(UserScope())
-        self+UserScope()
+        #self+UserScope()
         print('==1')
         print(self.allow_api)
 
@@ -22,6 +26,8 @@ class UserScope(Scope):
 
 class SuperScope(Scope):
     allow_api =['v1.xxxxxxx']
+    allow_module = ['v1.user']
+
     def __init__(self,):
         #self.add(UserScope()).add(AdminScope())
         self+UserScope()+AdminScope()
@@ -38,10 +44,17 @@ def is_in_scope(scope, endpoint):
     #  globals() 比较有意思，他返回的是一个字典，包含了当前的各种类啊什么的
     scope = globals()[scope]()
 
-    print(scope.allow_api)
+    #这里 scope.allow_api 保存了 "v1.view_func" 这样子的视图函数，我们需要改变它默认的，中间添加 module 的名字
+    #这样子为了权限控制的时候，对整个模块的下的视图函数的支持，而不用一个一个的添加。
+    #我们需要 scope.allow_api 改成 "v1.模块名字(也就是红图的名字)+view_func"这样的形式
+    splits = endpoint.split('+')
+    red_name = splits[0]
 
     if endpoint in scope.allow_api:
         return True
+    if red_name in scope.allow_module:
+        return  True
+
     else:
         return False
 
